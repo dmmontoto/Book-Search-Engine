bf17e6be925bc9ee4d13ab96fe5fd38aaa84007f
dmmontoto <dmontoto@ucsd.edu>
2023-11-18 17:03:18 -0800

    modified app.jsx for Apollo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+// Replace the URL with your Apollo Server URL
+const uri = 'YOUR_APOLLO_SERVER_URL';
+
+const client = new ApolloClient({
+  uri,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Outlet />
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
